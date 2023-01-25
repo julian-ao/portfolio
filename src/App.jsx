ef6dd1109772f857a8ebb52110fe5a52b3574fc1
Julian <julianao@stud.ntnu.no>
2023-01-25 16:54:13 +0100

    Changed background from p5 to vanilla js to reduce lag

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 
 import Background from './components/Background'
+import JSBackground from './components/JSBackground'
 import Navbar from './components/Navbar'
 import Intro from './components/Intro'
 import Projects from './components/Projects'
 import About from './components/About'
 import Footer from './components/Footer'
 
+import useWindowDimensions from './hooks/useWindowDimensions';
+
 const App = () => {
+  const windowWidth = useWindowDimensions().width
+  const windowHeight = useWindowDimensions().height
+
   return (
     <div className='font-inter overflow-hidden' >
       {/*<Background />*/}
+      <JSBackground windowWidth={windowWidth} windowHeight={windowHeight} />
       <React.StrictMode>
         <Navbar />
         <Intro />
---
+++ src/components/JSBackground.jsx
@@ -0,0 +1,162 @@
+import React, { useEffect, useRef } from 'react'
+
+const CANVAS_MULTIPLIER = 2
+
+const JSBackground = ({ windowWidth, windowHeight }) => {
+
+  const CANVAS_WIDTH = windowWidth * CANVAS_MULTIPLIER // times CANVAS_MULTIPLIER to fix canvas cutting short on upscaling
+  const CANVAS_HEIGHT = windowHeight * CANVAS_MULTIPLIER
+
+  let canvasRef = useRef(null)
+
+  let bubbles = []
+
+  const COLORS = ["#E5B8F4", "#d2a1e3"] // "#810CA8""#C147E9", 
+  const BACKGROUND_COLOR = "rgba(52, 24, 61, 1)" // rgba(45, 3, 59, 1)
+
+  const AMOUNT_OF_BUBBLES = 20
+  const BLUR_PIXELS = 0
+
+  const MIN_SPEED = 1
+  const MAX_SPEED = 2
+  const MAX_SIDE_SPEED = 1
+
+  const MIN_RADIUS = CANVAS_WIDTH / 50
+  const MAX_RADIUS = CANVAS_WIDTH / 20
+
+  useEffect(() => {
+    // create canvas
+    const canvas = document.createElement("canvas");
+    canvas.width = CANVAS_WIDTH;
+    canvas.height = CANVAS_HEIGHT;
+    canvasRef.current.appendChild(canvas);
+
+    const ctx = canvas.getContext("2d");
+
+    // create bubbles
+    class Bubble {
+      show() {
+        ctx.fillStyle = COLORS[this.colorIndex]
+        ctx.beginPath()
+        ctx.ellipse(this.x, this.y, this.radius * 2, this.radius * 2, 0, 0, Math.PI * 2);
+        ctx.fill()
+      }
+
+      startSpawn() {
+        this.spawnPoint = Math.ceil(randomIntFromInterval(0, 4))
+        this.colorIndex = randomIntFromInterval(0, COLORS.length - 1)
+
+        this.radius = randomIntFromInterval(MIN_RADIUS, MAX_RADIUS)
+        this.sideVelocity = Math.random()*2*MAX_SIDE_SPEED - MAX_SIDE_SPEED // ?
+        this.speed = randomIntFromInterval(MIN_SPEED, MAX_SPEED)
+
+        this.x = randomIntFromInterval(0, CANVAS_WIDTH)
+        this.y = randomIntFromInterval(0, CANVAS_HEIGHT)
+      }
+
+      respawn() {
+        this.spawnPoint = Math.ceil(randomIntFromInterval(0, 4))
+        this.colorIndex = randomIntFromInterval(0, COLORS.length - 1)
+
+        this.radius = randomIntFromInterval(MIN_RADIUS, MAX_RADIUS)
+        this.sideVelocity = Math.random()*2*MAX_SIDE_SPEED - MAX_SIDE_SPEED // ?
+        this.speed = randomIntFromInterval(MIN_SPEED, MAX_SPEED)
+
+        if (this.spawnPoint == 1) { // TOP
+          this.x = randomIntFromInterval(this.radius, CANVAS_WIDTH - this.radius)
+          this.y = - this.radius - BLUR_PIXELS
+
+        } else if (this.spawnPoint == 2) { // RIGHT
+          this.x = CANVAS_WIDTH + this.radius + BLUR_PIXELS
+          this.y = randomIntFromInterval(this.radius, CANVAS_HEIGHT)
+
+        } else if (this.spawnPoint == 3) { // BOTTOM
+          this.x = randomIntFromInterval(this.radius, CANVAS_WIDTH - this.radius)
+          this.y = CANVAS_HEIGHT + this.radius + BLUR_PIXELS
+
+        } else if (this.spawnPoint == 4) { // TOP
+          this.y = randomIntFromInterval(this.radius, CANVAS_HEIGHT - this.radius)
+          this.x = - this.radius - BLUR_PIXELS
+        }
+      }
+
+      move() {
+        if (this.spawnPoint == 1) { // TOP
+          this.y += this.speed
+          this.x += this.sideVelocity
+
+          if (this.y > CANVAS_HEIGHT + this.radius + BLUR_PIXELS ||
+              this.x - this.radius - BLUR_PIXELS > CANVAS_WIDTH ||
+              this.x < - this.radius - BLUR_PIXELS) { // if outside of canvas
+            this.respawn()
+          }
+
+        } else if (this.spawnPoint == 2) { // RIGHT
+          this.x -= this.speed
+          this.y += this.sideVelocity
+
+          if (this.y > CANVAS_HEIGHT + this.radius + BLUR_PIXELS ||
+              this.x < - this.radius - BLUR_PIXELS||
+              this.y < - this.radius - BLUR_PIXELS) { // if outside of canvas
+            this.respawn()
+          }
+
+        } else if (this.spawnPoint == 3) { // BOTTOM
+          this.y -= this.speed
+          this.x += this.sideVelocity
+
+          if (this.x - this.radius - BLUR_PIXELS > CANVAS_WIDTH ||
+              this.x < - this.radius - BLUR_PIXELS||
+              this.y < - this.radius - BLUR_PIXELS) { // if outside of canvas
+            this.respawn()
+          }
+
+        } else if (this.spawnPoint == 4) { // LEFT
+          this.x += this.speed
+          this.y += this.sideVelocity
+
+          if (this.y > CANVAS_HEIGHT + this.radius + BLUR_PIXELS ||
+              this.x - this.radius - BLUR_PIXELS > CANVAS_WIDTH ||
+              this.y < - this.radius - BLUR_PIXELS) { // if outside of canvas
+            this.respawn()
+          }
+
+        }
+
+      }
+    }
+
+    // animation loop
+    function animate() {
+      requestAnimationFrame(animate)
+      ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+
+      // drawing code
+      ctx.fillStyle = BACKGROUND_COLOR
+      ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+
+      while (bubbles.length < AMOUNT_OF_BUBBLES) {
+        bubbles.push(new Bubble())
+        bubbles[bubbles.length - 1].startSpawn()
+      }
+
+      for (let i = 0; i <= bubbles.length - 1; i++) {
+        bubbles[i].move()
+        bubbles[i].show()
+      }
+    }
+
+    animate()
+
+  }, [windowWidth, windowHeight])
+
+  const randomIntFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+  return (
+    <div ref={canvasRef} className='fixed -z-10 blur-4xl scale-150'/>
+  )
+}
+
+export default JSBackground
