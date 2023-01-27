7bcb121b1ac5a129f674e5ec730cef3bc4c468be
Julian <julianao@stud.ntnu.no>
2023-01-27 17:00:35 +0100

    Sike

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -29,7 +29,7 @@ const Project = (props) => {
 
   return (
     <div className='group bg-darkGrey h-72 rounded-2xl bg-cover drop-shadow-xl
-      hover:drop-shadow-none duration-200 cursor-pointer overflow-hidden'
+      hover:drop-shadow-none duration-200 cursor-pointer '
       style={{ backgroundImage: `url(${props.imageUrl})` }}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave} >
